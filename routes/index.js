e4b53fac03227ab3f6910537a4c7a4a6e85983c7
Thiago Paes <mrprompt@gmail.com>
2017-10-15 13:06:18 -0200

    add pagina

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,7 @@ exports = module.exports = function (app) {
 	app.get('/clientes', routes.views.clients);
 	app.all('/contato', routes.views.contact);
 	app.all('/page/:uri', routes.views.page);
+	app.all('/pagina/:uri', routes.views.page);
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
