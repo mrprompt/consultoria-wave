cb04e63c7e52aae31a1d389a6b6955f80aa3ef0b
Thiago Paes <mrprompt@gmail.com>
2020-03-23 23:58:37 -0300

    test https

--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -179,7 +179,7 @@ module.exports = function () {
 		if ((context) && (context.public_id)) {
 			options.hash.secure = keystone.get('cloudinary secure') || false;
 			var imageName = context.public_id.concat('.', context.format);
-			return cloudinary.url(imageName, options.hash);
+			return cloudinary.url(imageName, options.hash).replace('http', 'https');
 		}
 		else {
 			return null;
