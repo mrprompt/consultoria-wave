52d26f07aefe19e9735c4f862ae3148bcf72b12d
Thiago Paes <mrprompt@gmail.com>
2020-07-30 02:11:42 -0300

    Add youtube videos

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,18 +1,35 @@
-var keystone = require('keystone');
+const keystone = require('keystone');
+const Parser = require('rss-parser');
+const parser = new Parser();
+const feedUrl = 'https://www.youtube.com/feeds/videos.xml?channel_id=' + process.env.YOUTUBE_CHANNEL_ID;
 
 exports = module.exports = function (req, res) {
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+	const view = new keystone.View(req, res);
+	const locals = res.locals;
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
+	// locals.section is used to set the currently selected item in the header navigation.
 	locals.section = 'home';
 
 	// find the portfolio
 	view.query('portfolio', keystone.list('Gallery').model.findOne({ key: 'trabalhos-recentes' }));
 	view.query('slides', keystone.list('Gallery').model.findOne({ key: 'slides' }));
 
-	// Render the view
-	view.render('index');
+	// get videos from youtube.
+	(async () => {
+		let feed = await parser.parseURL(feedUrl);
+		let videos = [];
+
+		feed.items.forEach(item => {
+			videos.push({
+				titulo: item.title,
+				link: item.link,
+				data: item.pubDate,
+				imagem: 'https://img.youtube.com/vi/' + item.id.replace('yt:video:', '') + '/hqdefault.jpg',
+			});
+		});
+
+		// Render the view
+		view.render('index', { videos });
+	})();
 };
